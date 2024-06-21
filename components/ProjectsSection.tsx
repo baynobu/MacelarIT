1aec600b5dfa90c0cfc09bd6398052b17995fb5d
baynobu <darmawanhanif30@gmail.com>
2024-06-21 20:43:12 +0700

    initial commit

---
+++ components/ProjectsSection.tsx
@@ -0,0 +1,128 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import SlideUp from "./SlideUp";
+
+const ProjectsSection = () => {
+  const projects = [
+    {
+      name: "The ABC Buddy",
+      description: "",
+      image: "/",
+      video: "https://www.youtube.com/embed/2kyABOgFhg4",
+      github: "https://github.com/hqasmei/thankful-thoughts",
+      link: "/MalangYoungInnovatorsAward-Certificate.jpg",
+    },
+    {
+      name: "Data Insights: Patterns, Predictions, and Decisions",
+      description: "",
+      image: "/data1.png",
+      github: "https://colab.research.google.com/",
+      link: "/AI_SMK NEGERI 12_ABDUL RAHMAN HANIF DARMAWAN.jpg",
+    },
+    {
+      name: "2D Pixel Platformer",
+      description:
+        "",
+      image: "/allienparkour.png",
+      github: "https://github.com/hqasmei/katorfamilyphotos",
+      link: "https://drive.google.com/drive/folders/1DCwJKesXjl_y5Y4iYyaIRBns_FTWnLPm?usp=drive_link",
+    },
+    {
+      name: "Personal Website",
+      description:
+        "",
+      image: "/personal_website.png",
+      github: "https://github.com/hqasmei/katorfamilyphotos",
+      link: "https://robclintonkardinal.com/",
+    },
+  ];
+
+  return (
+    <section id="projects">
+      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-10 md:py-10 md:flex-row md:space-x-4 md:text-left"></div>
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded" />
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className="md:w-1/2">
+                    {project.video ? (
+                      <div className="aspect-w-16 aspect-h-9">
+                        <iframe
+                          src={project.video}
+                          title={project.name}
+                          allowFullScreen
+                          width={465}
+                          height={250}
+                          className="rounded-xl shadow-xl hover:opacity-70"
+                        />
+                      </div>
+                    ) : (
+                      <Link href={project.link}>
+                        <Image
+                          src={project.image}
+                          alt=""
+                          width={1000}
+                          height={1000}
+                          className="rounded-xl shadow-xl hover:opacity-70"
+                        />
+                      </Link>
+                    )}
+                  </div>
+
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    {project.name === "The ABC Buddy" && (
+                      <p className="text-base leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                        Permainan Edukasi Multi-Sensori untuk Disleksia. ABC Buddy dipamerkan di Malang Young Innovators Award (MYIA) 2023 pada tanggal 21 Mei 2023, 
+                        di Auditorium MCC, Kota Malang.{" "}
+                        <br />
+                      </p>
+                    )}
+                    
+                    {project.name === "Data Insights: Patterns, Predictions, and Decisions" && (
+                      <p className="text-base leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                        Proyek ini dibuat untuk menyelesaikan sebuah Studi Kasus sebuah Perusahaan Elektronik Internet yang ingin memprediksi harga jual Produk dengan SKU (TLWR840N) 
+                        pada Platfrom Tokopedia, dan Proyek ini menggunakan model Regresi Linear. {" "}
+                        <br />
+                      </p>
+                    )}
+                    
+
+                    {project.name === "2D Pixel Platformer" && (
+                      <p className="text-base leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                        Alien Parkour, sebuah permainan platformer 2D yang dibuat dengan Godot Game Engine.{" "}
+                        <br />
+                      </p>
+                    )}
+
+                    {project.name === "Personal Website" && (
+                      <p className="text-base leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                        Pembuatan Personal Website Rob Clinton Kardinal, dan terdapat beberapa fitur utama yaitu Galeri Foto, Blog Terintegrasi, dan serta Panel Admin.{" "}
+                        <br />
+                        <a href={project.link} className="text-teal-500 hover:underline" target="blank">
+                          View Live Website!
+                        </a>
+                        .
+                      </p>
+                    )}
+
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsSection;
